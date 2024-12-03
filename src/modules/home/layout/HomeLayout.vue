112e1696f5686b1449de7ac2d6cdbd4b1aaae4ea
Dounder <dr.glasdou@gmail.com>
2024-12-03 10:33:48 -0600

    feat: enhance HomeLayout with improved padding and max-width for better layout responsiveness

--- src/modules/home/layout/HomeLayout.vue
+++ src/modules/home/layout/HomeLayout.vue
@@ -7,8 +7,10 @@ import MainNavbar from '../components/MainNavbar.vue'
     <div class="flex flex-col flex-grow min-h-0 min-w-0">
       <MainNavbar />
 
-      <div class="flex-grow p-6 overflow-auto dark:bg-dark-800 min-w-0">
-        <router-view />
+      <div class="flex-grow overflow-auto dark:bg-dark-800 min-w-0">
+        <div class="max-w-[1700px] w-full mx-auto p-6">
+          <router-view />
+        </div>
       </div>
     </div>
   </div>
