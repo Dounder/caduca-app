42928015c8e260f353007022665dc438d3216e21
glasdou.dev <dr.glasdou@gmail.com>
2025-01-21 12:07:16 -0600

    feat: enhance login view with loading spinner and error handling

--- src/modules/auth/views/LoginView.vue
+++ src/modules/auth/views/LoginView.vue
@@ -6,11 +6,12 @@ import { useI18n } from 'vue-i18n'
 import { envs } from '@/config/envs'
 import { useNotification } from '@/modules/shared'
 import CustomButton from '@/modules/shared/components/CustomButton.vue'
+import CustomCard from '@/modules/shared/components/CustomCard.vue'
+import CustomSpinner from '@/modules/shared/components/CustomSpinner.vue'
 import CustomInputPassword from '@shared/components/CustomInputPassword.vue'
 import CustomInputText from '@shared/components/CustomInputText.vue'
 import { loginSchema } from '../schemas'
 import { useAuthStore } from '../store/auth.store'
-import CustomCard from '@/modules/shared/components/CustomCard.vue'
 
 const { t } = useI18n()
 const { showSuccess, showError } = useNotification()
@@ -22,11 +23,14 @@ const { defineField, errors, handleSubmit } = useForm({
   initialValues
 })
 
+const loading = ref(false)
 const error = ref<string | null>(null)
 const [username, usernameAttrs] = defineField('username')
 const [password, passwordAttrs] = defineField('password')
 
 const onSubmit = handleSubmit(async ({ username, password }, { resetForm }) => {
+  loading.value = true
+
   try {
     const ok = await authStore.login(username, password)
 
@@ -41,6 +45,8 @@ const onSubmit = handleSubmit(async ({ username, password }, { resetForm }) => {
   } catch (error) {
     console.log(error)
     showError({ detail: t('auth.login.error') })
+  } finally {
+    loading.value = false
   }
 })
 </script>
@@ -48,7 +54,7 @@ const onSubmit = handleSubmit(async ({ username, password }, { resetForm }) => {
 <template>
   <div class="max-w-[30rem] h-full mx-auto flex items-center flex-col justify-center">
     <h1 class="font-bold text-center text-2xl mb-5">Logo</h1>
-    <CustomCard class="w-full">
+    <CustomCard class="w-full relative">
       <h4 class="text-center text-2xl mb-6">{{ t('auth.login.title') }}</h4>
       <form @submit="onSubmit" class="flex flex-col gap-6" v-focustrap>
         <CustomInputText
@@ -71,6 +77,9 @@ const onSubmit = handleSubmit(async ({ username, password }, { resetForm }) => {
           <Message v-if="error" severity="error">{{ error }}</Message>
         </transition>
       </form>
+      <div v-if="loading" class="absolute inset-0 z-50 flex items-center justify-center bg-black/50">
+        <CustomSpinner />
+      </div>
     </CustomCard>
   </div>
 </template>
